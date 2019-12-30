30d1d0a532359e195584478fcaaaf4dccf7ddf5e
Dominik Laso <lasododo123@gmail.com>
2019-12-30 22:06:22 +0100

    first commit

---
+++ lets_play_ujo_ansible/python_file.py
@@ -0,0 +1,8 @@
+print("\n****************************")
+print("\nHello World from python :)")
+print("\n****************************\n")
+
+with open("hello.txt", "a") as f:
+    f.write("\n****************************")
+    f.write("\nHello World from python :)")
+    f.write("\n****************************\n")
